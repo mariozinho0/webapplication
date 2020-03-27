457505fc99203a4c0366d635b3fd0f9865173645
Pablo <pablo.lima.araujo@gmail.com>
2020-03-27 01:38:56 -0300

    arrumando css e javascript

--- src/main/java/br/com/gopark/conf/AppWebConfiguration.java
+++ src/main/java/br/com/gopark/conf/AppWebConfiguration.java
@@ -6,13 +6,22 @@ import org.springframework.format.datetime.DateFormatter;
 import org.springframework.format.datetime.DateFormatterRegistrar;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @EnableWebMvc
 /*Adicionar Controllers e DAO aqui*/
 @ComponentScan(basePackages = {"br.com.gopark.controller", "br.com.gopark.dao"})
-public class AppWebConfiguration {
+public class AppWebConfiguration implements WebMvcConfigurer {
+
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
 
     @Bean
     public InternalResourceViewResolver internalResourceViewResolver() {
